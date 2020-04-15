2d09a4ea9796af9becd2d3125edb0c537c1df83a
Manniec <manniec00@gmail.com>
2020-04-15 02:55:16 -0700

    lab_8

--- Lab_8/recursion.s
+++ Lab_8/recursion.s
@@ -15,7 +15,7 @@ main: 		addi $sp, $sp, -4	# Moving stack pointer to make room for storing local 
 
 		li $v0,5       #string output syscall
 		syscall
-		move $t3,$v0    #save value in $v0 to $a0 to be argument for recursion func
+		move $a0,$v0    #save value in $v0 to $a0 to be argument for recursion func
 		
 		jal recursion	# Call recursion(x)
 		
@@ -33,48 +33,55 @@ recursion:	addi $sp, $sp, -12	# Push stack frame for local storage
 
 		# TPS 2 #7 
 		
+		sw $ra, 8($sp)		#save return adress
 		
-		
-		addi $t0, $a0, 1
-		bne $t0, $zero, not_minus_one
+		addi $t0, $a0, 1	#t0 is input + 1
+		bne $t0, $zero, not_minus_one	#Check if $a0 was -1
 		
 		# TPS 2 #8 (update returned value)
+		
+		addi $v0, $zero, 1	#return 1 if $a0 was -1
 		
 		
 		j end_recur
 			
-not_minus_one:	bne $a0, $zero, not_zero
+not_minus_one:	bne $a0, $zero, not_zero #check if input was zero
 
 		# TPS 2 #9 (update returned value)
 		
+		addi $v0, $zero, 3 	#return 3 if is zero
 		
 		j end_recur		
 
-not_zero:	sw $a0, 4($sp) 	
+not_zero:	sw $a0, 4($sp) 		#save input before jumping to recursion again
 		# TPS 2 #11 (Prepare new input argument, i.e. m - 2)
 		
-		
+		addi $a0, $a0, -2
 		
 		jal recursion	# Call recursion(m - 2)
 		
 		# TPS 2 #12 
 
-		
+		sw $v0, 0($sp)
 		
 		# TPS 2 #13 (Prepare new input argument, i.e. m - 1)
 		
+		lw $a0, 4($sp)
+		addi $a0, $a0, -1
 		
 		jal recursion	# Call recursion(m - 1)
 		
 		# TPS 2 #14 (update returned value)
 		
+		lw $t0, 0($sp)
+		add $v0, $v0, $t0
 		
 		j end_recur
 		
 
 # End of recursion function	
 end_recur:	# TPS 2 #15 
-
+		lw $ra, 8($sp)
 		addi $sp, $sp, 12	# Pop stack frame 
 		jr $ra
 
